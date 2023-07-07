6facd6c4683a8e06cb6ec144912e9aa7bffe25e6
mrrahulkaran <karanr9991@gmail.com>
2023-07-07 09:45:07 +0530

    data typ initilization

---
+++ datatyp/javadata.java
@@ -0,0 +1,24 @@
+package datatyp;
+
+public class javadata {
+
+	public static void main(String[] args) {
+
+
+     
+
+		int myInt = 9;
+	   
+		double myDouble = myInt; 
+	    System.out.println(myInt);     
+	    System.out.println(myDouble);
+	    
+	  
+	    double myDouble1 = 9.78d;
+	    int myInt1 = (int) myDouble1; 
+
+	    System.out.println(myDouble1);  
+	    System.out.println(myInt1);
+	}
+
+}
---
+++ datatyp/primitive.java
@@ -0,0 +1,27 @@
+package datatyp;
+
+public class primitive {
+
+	public static void main(String[] args)
+	 
+	int nub1=20;
+	
+	byte a=10;
+	
+	short s=10000;
+	
+	long l=100000L;
+	
+	double d=12.3;
+	
+	char letter='a';
+	
+	
+	System.out.println
+	
+	
+	
+	 
+	
+	
+}
